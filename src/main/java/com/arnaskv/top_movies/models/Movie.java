97ea0539ac2185a99c24310f5effc357157bcef7
Arnas Kvedaravicius <arnaskw@gmail.com>
2024-07-01 18:54:25 +0300

    feat: movie; genre; many to many relationship migrations

--- src/main/java/com/arnaskv/top_movies/models/Movie.java
+++ src/main/java/com/arnaskv/top_movies/models/Movie.java
@@ -1,7 +1,9 @@
 package com.arnaskv.top_movies.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +29,7 @@ public class Movie {
     @Column(nullable = false)
     private Float rating;
 
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
         name = "movie_genre",
         joinColumns = @JoinColumn(name = "movie_id"),
